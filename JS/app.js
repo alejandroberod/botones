ed76862c6c46e45cef483877693028037e01fcf2
alejandroberod <119709936+alejandroberod@users.noreply.github.com>
2023-05-03 17:36:18 -0500

    Cambio de código JS

---
+++ JS/app.js
@@ -0,0 +1,33 @@
+a = document.querySelector('.btn-14')
+a.addEventListener("mouseover", () => {
+    document.querySelector('.btn-1').classList.add('ch-1');
+    document.querySelector('.btn-2').classList.add('ch-2');
+    document.querySelector('.btn-3').classList.add('ch-3');
+    document.querySelector('.btn-4').classList.add('ch-4');
+    document.querySelector('.btn-5').classList.add('ch-5');
+    document.querySelector('.btn-6').classList.add('ch-6');
+    document.querySelector('.btn-7').classList.add('ch-7');
+    document.querySelector('.btn-8').classList.add('ch-8');
+    document.querySelector('.btn-9').classList.add('ch-9');
+    document.querySelector('.btn-10').classList.add('ch-10');
+    document.querySelector('.btn-11').classList.add('ch-11');
+    document.querySelector('.btn-12').classList.add('ch-12');
+    document.querySelector('.btn-13').classList.add('ch-13');
+    document.querySelector('.btn-15').classList.add('ch-15');
+})
+a.addEventListener("mouseout", () => {
+    document.querySelector('.btn-1').classList.remove('ch-1');
+    document.querySelector('.btn-2').classList.remove('ch-2');
+    document.querySelector('.btn-3').classList.remove('ch-3');
+    document.querySelector('.btn-4').classList.remove('ch-4');
+    document.querySelector('.btn-5').classList.remove('ch-5');
+    document.querySelector('.btn-6').classList.remove('ch-6');
+    document.querySelector('.btn-7').classList.remove('ch-7');
+    document.querySelector('.btn-8').classList.remove('ch-8');
+    document.querySelector('.btn-9').classList.remove('ch-9');
+    document.querySelector('.btn-10').classList.remove('ch-10');
+    document.querySelector('.btn-11').classList.remove('ch-11');
+    document.querySelector('.btn-12').classList.remove('ch-12');
+    document.querySelector('.btn-13').classList.remove('ch-13');
+    document.querySelector('.btn-15').classList.remove('ch-15');
+})
